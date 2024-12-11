5e4b866759236b208e7f76d4133462b5d52fbb9c
Tommy Le <tommy.le@student.careeria.fi>
2024-12-11 21:05:27 +0200

    Add project files.

---
+++ Harjoitus2_Opiskelija/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Harjoitus2_Opiskelija
+{
+    class Progam
+    { 
+    static void Main(string[] args)
+    {
+        //luodaan Opiskelija olio
+        Opiskelija opiskelija1 = new Opiskelija("Matti", "TVT1234", 0);
+
+        opiskelija1.TulostaData();
+        opiskelija1.MuokkaaOpintopisteitä(5);
+        opiskelija1.TulostaData();
+
+        }
+    }
+}
